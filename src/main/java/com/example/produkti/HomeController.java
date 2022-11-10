189782c08fb1d44c0bf181f4a56af6adc9213cf0
GROUPAD1\pikseri <rinalds.pikse@intrum.com>
2022-11-10 12:22:49 +0200

    gradle to maven change

--- src/main/java/com/example/produkti/HomeController.java
+++ src/main/java/com/example/produkti/HomeController.java
@@ -1,7 +1,7 @@
 package com.example.produkti;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
--- src/main/java/com/example/produkti/ProduktiApplication.java
+++ src/main/java/com/example/produkti/ProduktiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProduktiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProduktiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProduktiApplication.class, args);
+    }
 
 }
