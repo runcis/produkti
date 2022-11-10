8622247cf55002a920f52715744b65cafd248bf9
GROUPAD1\pikseri <rinalds.pikse@intrum.com>
2022-11-10 15:36:19 +0200

    Add gcloud databse connection

--- src/main/java/com/example/produkti/HomeController.java
+++ src/main/java/com/example/produkti/HomeController.java
@@ -1,12 +1,30 @@
 package com.example.produkti;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 public class HomeController {
 
+    private final JdbcTemplate jdbcTemplate;
+
+    public HomeController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @GetMapping("/getVeikali")
+    public List<String> getTuples() {
+        return this.jdbcTemplate.queryForList("SELECT * FROM veikali").stream()
+                .map((m) -> m.values().toString())
+                .collect(Collectors.toList());
+    }
 
     @RequestMapping("/")
     public ModelAndView home () {
